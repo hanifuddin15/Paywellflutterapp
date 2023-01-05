242e5e9007368182406e00446e5811c213125c6d
hanifuddin15 <hanifuddin.buet@outlook.com>
2023-01-06 00:20:57 +0600

    title removed

--- paywell/lib/main.dart
+++ paywell/lib/main.dart
@@ -13,7 +13,7 @@ class Myapp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: LoginPage(title: ''),
+      home: LoginPage(),
       debugShowCheckedModeBanner: false,
     );
   }
